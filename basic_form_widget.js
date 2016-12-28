331c86c3fe5d26478149437d88d478cefd5af6ff
shurnster <--replace-all>
2016-12-27 16:09:18 -0800

    $.widget('Shurns.basicForm', {
      options: {
        itemNames: [],
        perColumn: 2
      },
    
      _create: function() {
        this._createData();
        this.element.append(this.uiFormBasic);
      },
    
      _columnToClass: function () {
    
        var column = this.options.perColumn;
    
        //Four columns per row is the maximum amount of columns to be inserted.
        var cols = {
          1: 'twelve',
          2: 'six',
          3: 'four',
          4: 'three'
        }
    
        return cols[column] + ' columns';
    
        // ex. this._addClass(this._div, column);
      },
    
      _createData: function () {
        var inputNames = this.options.itemNames,
            formData = '';
    
        this.uiFormBasic = $('<div>');
        this._addClass(this.uiFormBasic, 'form-default');
    
        if ($.isArray(inputNames) && inputNames.length) {
          $.each(inputNames, function (num, names) {
            formData += '<label>' + names + ':</label><input type="text">';
          });
        }
    
        this.uiFormBasic.append(formData);
    
        this._addClass(this.uiFormBasic.find('label'), 'form-default', 'labels');
        this._addClass(this.uiFormBasic.find('input'), 'form-default');
      },
    
      _setOption: function(key, value) {
    
      },
    
      _appendRows: function() {
        //Each row should append nth elements from the "perColumn" option.
      }
    });
    
    $(document).ready(function () {
      $('#divTest').basicForm({
        itemNames: ['First Name', 'Last Name', 'Occupation']
      });
    });
    
    modified:   basic_form_widget.js

--- basic_form_widget.js
+++ basic_form_widget.js
@@ -1,37 +1,62 @@
-/// <reference path="jquery-1.12.4.js" />
-/// <reference path="jquery-ui-1.8.16.custom.min.js" />
-
-$.widget('Shurns.simpleValid', {
+$.widget('Shurns.basicForm', {
 
   options: {
-    itemNames: []
+    itemNames: [],
+    perColumn: 2
   },
 
   _create: function () {
+    this._createData();
+    this.element.append(this.uiFormBasic);
+  },
 
-    if (this.options.itemNames) {
-      this._addClass(this.element, 'form-default');
-      this._createItems();
+  _columnToClass: function () {
+
+    var column = this.options.perColumn;
+
+    //Four columns per row is the maximum amount of columns to be inserted.
+    var cols = {
+      1: 'twelve',
+      2: 'six',
+      3: 'four',
+      4: 'three'
     }
+
+    return cols[column] + ' columns';
+
+    // ex. this._addClass(this._div, column);
   },
 
-  _createDivWrapper: function () {
+  _createData: function () {
+    var inputNames = this.options.itemNames,
+        formData = '';
+
+    this.uiFormBasic = $('<div>');
+    this._addClass(this.uiFormBasic, 'form-default');
+
+    if ($.isArray(inputNames) && inputNames.length) {
+      $.each(inputNames, function (num, names) {
+        formData += '<label>' + names + ':</label><input type="text">';
+      });
+    }
+
+    this.uiFormBasic.append(formData);
+
+    this._addClass(this.uiFormBasic.find('label'), 'form-default', 'labels');
+    this._addClass(this.uiFormBasic.find('input'), 'form-default');
+  },
+
+  _setOption: function (key, value) {
 
   },
 
-  _createItems: function () {
-    var items = this.options.itemNames,
-        textBox;
-
-    $.each(items, function (index, item) {
-
-      textBox = $('<input>', { type: 'text' }).appendTo(this.element);
-      this._addClass(textBox, 'form-default', 'form-textbox');
-
-    }.bind(this));
+  _appendRows: function () {
+    //Each row should append nth elements from the "perColumn" option.
   }
 });
 
 $(document).ready(function () {
-  //Simple Validation Widget goes here...
+  $('#divTest').basicForm({
+    itemNames: ['First Name', 'Last Name', 'Occupation']
+  });
 });
