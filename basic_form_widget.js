e46aa7c30b70b0f72c8051ffb3271904a81d1b62
shurnster <slainuponmyshield@gmail.com>
2017-01-13 23:44:31 -0800

    Refactored code

--- basic_form_widget.js
+++ basic_form_widget.js
@@ -17,10 +17,10 @@ $.widget('Shurns.basicForm', {
 
   _createData: function () {
     var inputNames = this.options.itemNames,
-    columns = this.options.perColumn,
-    formData = '',
-    storeInputs = [],
-    x = 0;
+        columns = this.options.perColumn,
+        formData = '',
+        storeInputs = [],
+        x = 0;
 
     this.uiFormBasic = $('<div>');
     this._addClass(this.uiFormBasic, 'form-default');
@@ -40,38 +40,25 @@ $.widget('Shurns.basicForm', {
   _columnToClass: function () {
 
     var column = this.options.perColumn,
-    uiFormBasic = this.uiFormBasic,
-    $rows = uiFormBasic.find('div.rows'),
-    $lastRow,
-    $prevRows;
+        uiFormBasic = this.uiFormBasic.find('div.rows'),
+        $prevRows = uiFormBasic.last().prevAll(),
+        $lastRow = uiFormBasic.last(),
+        cols = {1: 'twelve', 2: 'six', 3: 'four', 4: 'three'},
+        $sizeEl = $lastRow.children().length < $prevRows.children().length ? cols[$lastRow.children().length] : cols[column];    
+    
 
-    //Four columns per row is the maximum amount of columns to be inserted.
-    var cols = {
-      1: 'twelve',
-      2: 'six',
-      3: 'four',
-      4: 'three'
-    }
-
-    if($rows.length > 1) {
-      $lastRow = $rows.last();
-      $prevRows = $lastRow.prevAll();
-
-      if($lastRow.children().length < $prevRows.children().length) {
-        this._addClass($lastRow.find('div'), 'form-default', cols[$lastRow.children().length] + ' columns');
-        this._addClass($lastRow.find('label'), 'form-default', 'labels');
-        this._addClass($lastRow.find('input'), 'form-default');
-      }
-     
-      this._addClass($prevRows.find('div'), 'form-default', cols[column] + ' columns');
-      this._addClass($prevRows.find('label'), 'form-default', 'labels');
-      this._addClass($prevRows.find('input'), 'form-default');
-          
-    }
-    this._addClass(uiFormBasic.find('div.rows').find('div'), 'form-default', cols[column] + ' columns');
-    this._addClass(uiFormBasic.find('label'), 'form-default', 'labels');
+    this._addClass(uiFormBasic.find('label'), 'labels');
     this._addClass(uiFormBasic.find('input'), 'form-default');
-    // ex. this._addClass(this._div, column);
+    
+    if(uiFormBasic.length > 1) {
+      this._addClass($prevRows.children(), cols[column] + ' columns');
+
+      //Bottom column(s) needs to match the same previous elements via stretch.
+      this._addClass(uiFormBasic.last().children(), $sizeEl + ' columns');
+    }
+    else {
+      this._addClass(uiFormBasic.find('div'), cols[column] + ' columns');
+    }
   },
 
   _setOption: function (key, value) {
